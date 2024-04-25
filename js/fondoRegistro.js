0a3f1609ef495cf3a904aedec448766e038b9e0e
Nicocaci <nicko.caci@gmail.com>
2024-04-25 15:30:19 -0300

    coreccion a los fondoss

--- js/fondoRegistro.js
+++ js/fondoRegistro.js
@@ -1,13 +1,13 @@
-//CONSTANTE MAIN
-const mainRegistro = document.querySelector("#imgFondoRegistro");
+// //CONSTANTE MAIN
+// const mainRegistro = document.querySelector("#imgFondoRegistro");
 
-//CONSTANTE FONDOS
-const fondos = ["anden.jpg", "barrendero.jpg", "logoer.png", "escoba.jpg", "fotocepillos.jpg", "pincel.jpg"];
+// //CONSTANTE FONDOS
+// const fondos = ["anden.jpg", "barrendero.jpg", "logoer.png", "escoba.jpg", "fotocepillos.jpg", "pincel.jpg"];
 
-//CAMBIAMOS FONDOS ALEATORIAMENTE
-function cambiarFondoRegistro(fondos) {
-    let aleatorio = Math.floor(Math.random() * fondos.length)
-    mainRegistro.setAttribute("src", `../img/${fondos[aleatorio]}`);
-}
+// //CAMBIAMOS FONDOS ALEATORIAMENTE
+// function cambiarFondoRegistro(fondos) {
+//     let aleatorio = Math.floor(Math.random() * fondos.length)
+//     mainRegistro.setAttribute("src", `../img/${fondos[aleatorio]}`);
+// }
 
-window.onload = cambiarFondoRegistro(fondos);
+// window.onload = cambiarFondoRegistro(fondos);
